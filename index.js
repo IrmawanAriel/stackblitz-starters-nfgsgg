6e88c1f78515f21020c113059811e875b2173e5b
IrmawanAriel <158867264+IrmawanAriel@users.noreply.github.com>
2024-02-05 16:54:31 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let nama = `Ariel Thymoti Irmawan`;
+let umur = 22;
+let alamat = 'Jatimulya, Tambun Selatan ,Bekasi Timur, Jawa Barat';
+let hobi = 'Bermain Basket.';
+let pendidikan = 'Universitas Advent Indonesia, jurusan Teknik Informatika.';
+let pengalaman = 'Disaat beberapa bulan sebelum kuliah, saya tidak tahu apapun tentang IT, dan saya penasaran dengan apa sebenarnya yang di pelajari di dalam IT dan akhirnya saya mulai belajar lebih awal sebelum saya masuk kuliah. seiring berjalannya waktu sayapun semakin teratrik dengan algoritma pemrograman berbahsa c++. akhirnya saya pun belajar mendalam untuk algoritma pemrograman. selain saya menyukai algoritma program, saya juga suka dengan problem solving untuk sebuah program.';
+let keahlian = 'PHP, HTML, CSS, JS, C++, C#, Phyton, Laravel, React.js, Vue.js, AI. tetapi yang paling saya kuasai adalah PHP, HTML, CSS ,Vue.js , dan  laravel.';
+let belajar = 'untuk proses belajar tidak pernah, tetapi saat proses pengerjaan project atau tugas tentu iya.';
+let tujuan = 'Saya mencari sebuah tempat pembelajaran yang sesuai dengan passion saya dan tentu juga punya kredibelitas tinggi yang dapat saya percaya, dengan harapan bahwa ilmu yang saya punya dapat berkembang yang mana tentu dapat meningkatkan value diri saya sendiri. sehingga proses peraihan masadepan yang baik niscaya dapat di ringankan dengan masuknya saya di fazztrack.';
+let Alasan_diterima = 'jikalau memang misi dari bootcamp Fazztrack adalah To Build Life-Changing Education, maka ini adalah saat yang tepat bagi fazztrack untuk menaikan taraf dan value dari kehidupan saya, seorang mahasiswa tingkat akhir yang ingin menaikan value dan taraf hidupnya.';
+
+console.log(nama);
+console.log(umur += " Tahun.");
+console.log(alamat);
+console.log(hobi);
+console.log(pendidikan);
+console.log(pengalaman);
+console.log(keahlian);
+console.log(belajar);
+console.log(tujuan);
+console.log(Alasan_diterima);
+
+
+
